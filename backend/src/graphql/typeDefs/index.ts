eae3a486a9d52b4b896d8e85dc94d5260007b7cf
Jayden Lee <jaydeh.lee@gmail.com>
2023-11-11 15:10:33 -0600

    setup for next deploy

--- backend/src/graphql/typeDefs/index.ts
+++
@@ -1,7 +0,0 @@
-import userTypeDefs from './user';
-import conversationTypeDefs from './conversation'
-import messageTypeDefs from './messages'
-
-const typeDefs = [userTypeDefs, conversationTypeDefs, messageTypeDefs];
-
-export default typeDefs;
--- backend/src/util/functions.ts
+++
@@ -1,9 +0,0 @@
-import { ParticipantPopulated } from './types';
-
-
-export const userIsConversationParticipant = (
-  participants: Array<ParticipantPopulated>,
-  userId: string
-  ): boolean => {
-    return !!participants.find(participant => participant.userId === userId)
-}
